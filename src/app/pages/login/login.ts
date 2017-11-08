1a2d7ee097ddbadeeab959f18891b1c6d0d7be74
laixiangran <laixiangran@163.com>
2017-11-08 15:52:19 +0800

    点击登录按钮直接登录

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -6,10 +6,8 @@
 
 import { Component } from '@angular/core';
 import { MenuController, NavController, Platform } from 'ionic-angular';
-import { LoginService } from './login.service';
 import { TabsPage } from '../tabs/tabs';
 import { AuthService } from '../../services/auth.service';
-import { TipsService } from '../../services/tips.service';
 import { Network } from '@ionic-native/network';
 import { StatusBar } from '@ionic-native/status-bar';
 import { ConfigService } from '../../services/config.service';
@@ -27,8 +25,6 @@ export class LoginPage {
 				public network: Network,
 				public statusBar: StatusBar,
 				public config: ConfigService,
-				public loginService: LoginService,
-				public tips: TipsService,
 				public authService: AuthService,
 				public menu: MenuController) {
 	}
@@ -42,51 +38,7 @@ export class LoginPage {
 		this.menu.enable(true);
 	}
 
-	onLogin(isValid: boolean) {
+	onLogin() {
 		this.navCtrl.setRoot(TabsPage);
-		// if (isValid) {
-		// 	if (this.config.network) {
-		// 		this.loginService.login({
-		// 			username: this.user.username,
-		// 			password: Md5.hashStr(this.user.password)
-		// 		}).subscribe((serverData: any) => {
-		// 			if (serverData.status === 1 || serverData.status === -1 || serverData.status === 200) {
-		// 				this.authService.setToken(serverData.token).then(() => {
-		// 					this.loginService.getUserInfo().subscribe((data: ServerData) => {
-		// 						if (data.code === 'ok') {
-		// 							this.authService.setUserInfo(data.result).then(() => {
-		// 								this.navCtrl.setRoot(TabsPage);
-		// 							});
-		// 						}
-		// 					});
-		// 				});
-		// 			} else {
-		// 				this.tips.alert({
-		// 					title: '登录失败',
-		// 					message: '用户名或密码错误！',
-		// 					buttons: ['确定']
-		// 				});
-		// 			}
-		// 		}, (error: any) => {
-		// 			this.tips.alert({
-		// 				title: '登录失败',
-		// 				message: error,
-		// 				buttons: ['确定']
-		// 			});
-		// 		});
-		// 	} else {
-		// 		this.tips.alert({
-		// 			title: '无网络连接',
-		// 			message: '请检查网络是否断开了！',
-		// 			buttons: ['确定']
-		// 		});
-		// 	}
-		// } else {
-		// 	this.tips.alert({
-		// 		title: '温馨提示',
-		// 		message: '必须输入用户名和密码！',
-		// 		buttons: ['确定']
-		// 	});
-		// }
 	}
 }
