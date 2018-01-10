6daf275bf40fb42a10b0601bbe84b287d879f771
laixiangran <laixiangran@163.com>
2018-01-10 22:31:09 +0800

    优化代码

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -47,40 +47,38 @@ export class MapPage {
 	}
 
 	watchPosition() {
-		this.watchId = setInterval(() => {
-			navigator.geolocation.getCurrentPosition((position: Position) => {
-				const coordinate: number[] = [position.coords.longitude, position.coords.latitude];
-				const isPush: boolean = this.coordinates.every((c: number[]) => {
-					return c.join() !== coordinate.join();
-				});
-				if (isPush) {
-					this.coordinates.push(coordinate);
-					if (this.polyline) {
-						this.polyline.setPath(JSON.parse(JSON.stringify(this.coordinates)));
-					} else {
-						this.polyline = new this.amapComponent.eAMap.Polyline({
-							path: JSON.parse(JSON.stringify(this.coordinates)), // 设置线覆盖物路径
-							strokeColor: '#3366FF', // 线颜色
-							strokeOpacity: 1, // 线透明度
-							strokeWeight: 2, // 线宽
-							strokeStyle: 'solid', // 线样式
-							strokeDasharray: [10, 5], // 补充线样式
-						});
-						this.polyline.setMap(this.amap);
-					}
-					if (this.marker) {
-						this.amap.remove([this.marker]);
-					}
-					this.marker = new this.amapComponent.eAMap.Marker({
-						icon: './assets/images/map/loc.png',
-						position: [position.coords.longitude, position.coords.latitude]
+		navigator.geolocation.watchPosition((position: Position) => {
+			const coordinate: number[] = [position.coords.longitude, position.coords.latitude];
+			const isPush: boolean = this.coordinates.every((c: number[]) => {
+				return c.join() !== coordinate.join();
+			});
+			if (isPush) {
+				this.coordinates.push(coordinate);
+				if (this.polyline) {
+					this.polyline.setPath(JSON.parse(JSON.stringify(this.coordinates)));
+				} else {
+					this.polyline = new this.amapComponent.eAMap.Polyline({
+						path: JSON.parse(JSON.stringify(this.coordinates)), // 设置线覆盖物路径
+						strokeColor: '#3366FF', // 线颜色
+						strokeOpacity: 1, // 线透明度
+						strokeWeight: 2, // 线宽
+						strokeStyle: 'solid', // 线样式
+						strokeDasharray: [10, 5], // 补充线样式
 					});
-					this.marker.setMap(this.amap);
-					this.amap.setFitView([this.marker]);
+					this.polyline.setMap(this.amap);
 				}
-			}, (error: PositionError) => {
-				console.log(error);
-			}, this.options);
-		}, 30000);
+				if (this.marker) {
+					this.amap.remove([this.marker]);
+				}
+				this.marker = new this.amapComponent.eAMap.Marker({
+					icon: './assets/images/map/loc.png',
+					position: [position.coords.longitude, position.coords.latitude]
+				});
+				this.marker.setMap(this.amap);
+				this.amap.setFitView([this.marker]);
+			}
+		}, (error: PositionError) => {
+			console.log(error);
+		}, this.options);
 	}
 }
